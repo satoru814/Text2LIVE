5bd7b10ef5a7296347722620c5c41c11f4ff9699
satoru814 <sakamotosatoru93@gmail.com>
2022-07-26 17:55:48 +0900

    aa

--- Text2LIVE.py
+++ Text2LIVE.py
@@ -1,3 +1,4 @@
+from re import template
 from sched import scheduler
 import torch
 import torch.nn as nn
@@ -13,7 +14,7 @@ import utils
 from itertools import chain
 import matplotlib.pyplot as plt
 import numpy as np
-import clip
+from CLIP import clip
 import wandb
 from config import CFG
 import argparse
@@ -24,11 +25,13 @@ from PIL import Image
 import PIL
 
 import EditNet
+import madgrad
 
-class Text2Live():
+class Text2LIVE():
     def __init__(self, args):
         self.wandb = args.wandb
         self.save_weight = args.save_weight
+        self.log_picture = args.log_picture
         if torch.cuda.is_available():
             self.device="cuda"
         else:
@@ -37,20 +40,20 @@ class Text2Live():
         self.content_path = CFG.content_path
         self.img_size = CFG.img_size
         self.text = CFG.text
-        self.source = CFG.source
+        self.screen = CFG.screen
+        self.ROI = CFG.ROI
         self.step = CFG.step
         self.save_inference_path = CFG.save_inference_path
+        self.lambda_composition = CFG.lambda_composition
+        self.lambda_screen = CFG.lambda_screen
+        self.lambda_structure = CFG.lambda_structure
+        self.optimizer_params = CFG.optimizer_params
 
 
     def build_model(self):
         self.Net = EditNet.UNet().to(self.device)
-        self.optimizer = torch.optim.Adam(self.Net.parameters(), self.lr)
-        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=60, gamma=0.5)
-        self.VGG = models.vgg19(pretrained=True).features
-        self.VGG.to(self.device)
-        for parameter in self.VGG.parameters():
-            parameter.requires_grad_(False)
-
+        self.optimizer = madgrad.MADGRAD(self.Net.parameters(), **self.optimizer_params)
+        self.scheduler = torch.optim.lr_scheduler.ExponentialLR(self.optimizer, gamma=0.99)
 
     def train(self):
         #wandb
@@ -60,74 +63,103 @@ class Text2Live():
 
         with torch.no_grad():
             self.content_img = utils.load_image(self.content_path, self.img_size).to(self.device)
-            content_features = utils.get_features(utils.img_normalize_vgg(self.content_img, self.device), self.VGG)
-
+            self.green_img = torch.zeros_like(self.content_img)
+            self.green_img[0,1,:,:] = torch.ones(size=(self.content_img.shape[2], self.content_img.shape[3])).to(self.device)
             clip_model, preprocess = clip.load("ViT-B/32", self.device, jit=False)
+            # template_text_roi = self.ROI
+            tokens_roi = clip.tokenize(self.ROI).to(self.device)
+            text_features_roi = clip_model.encode_text(tokens_roi).detach()
+            text_features_roi /= text_features_roi.norm(dim=-1, keepdim=True)
+
+            # template_text_screen = self.screen
+            tokens_screen = clip.tokenize(self.screen).to(self.device)
+            text_features_screen = clip_model.encode_text(tokens_screen).detach()
+            text_features_screen /= text_features_screen.norm(dim=-1, keepdim=True)
+
+        augmentation = utils.get_transforms(self.img_size,)
+        for step in range(CFG.step):
+            print(step)
+            self.scheduler.step()
+            losses = {"total_loss":0, "composition_loss":0, "screen_loss":0, "structure_loss":0}
+            self.optimizer.zero_grad()
+            loss = 0
+            screen_loss = 0
+            structure_loss = 0
+            composition_loss = 0
+
+
+            self.content_img_aug = augmentation(self.content_img)
+            source_features = clip_model.encode_image(
+                utils.img_normalize_clip(self.content_img_aug, self.device)
+                )
+            source_features /= (source_features.clone().norm(dim=-1, keepdim=True))
             template_text_target = utils.compose_text_with_templates(self.text)
             tokens_features = clip.tokenize(template_text_target).to(self.device)
             text_features = clip_model.encode_text(tokens_features).detach()
-            text_features = text_features.mean(axis=0, keepdim=True)
             text_features /= text_features.norm(dim=-1, keepdim=True)
-
-            template_text_source = utils.compose_text_with_templates(self.source)
-            tokens_source = clip.tokenize(template_text_source).to(self.device)
-            text_source = clip_model.encode_text(tokens_source).detach()
-            text_source = text_source.mean(axis=0, keepdim=True)
-            text_source /= text_source.norm(dim=-1, keepdim=True)
-
-            source_features = clip_model.encode_image(utils.img_normalize_clip(self.content_img, self.device))
-            source_features /= (source_features.clone().norm(dim=-1, keepdim=True))
-
-        
-        random_patching  = utils.get_transforms_patch()
-        for step in range(CFG.step):
-            self.scheduler.step()
-            losses = {"total_loss":0, "patch_loss":0, "grobal_loss":0, "content_loss":0}
-            self.optimizer.zero_grad()
-            content_loss = 0
-            patch_loss = 0
-
-            self.Net.train()
-            self.target = self.Net(self.content_img)
-            self.target.requires_grad_(True)
-            target_features = utils.get_features(utils.img_normalize_vgg(self.target, self.device), self.VGG)
-            content_loss += torch.mean((target_features["conv4_2"]-content_features["conv4_2"])**2)
-            content_loss += torch.mean((target_features["conv5_2"]-content_features["conv5_2"])**2)
-
-            img_aug = []
-            for n in range(self.crop_n):
-                temp_patch = random_patching(self.target)
-                img_aug.append(temp_patch)
-            img_aug = torch.cat(img_aug, dim=0).to(self.device)
-            img_features = clip_model.encode_image(utils.img_normalize_clip(img_aug, self.device))
-            img_features /= img_features.clone().norm(dim=-1, keepdim=True)
-            img_direction = img_features - source_features
-            img_direction /= img_direction.clone().norm(dim=-1, keepdim=True)
-            
-            text_direction = (text_features - text_source).repeat(img_direction.size(0), 1)
+            text_direction = text_features - text_features_roi
             text_direction /= text_direction.norm(dim=-1, keepdim=True)
 
-            loss_temp = 1 - torch.cosine_similarity(img_direction, text_direction, dim=1)
-            # loss_temp[loss_temp<self.patch_threshold] = 0
-            patch_loss = loss_temp.mean()
+            #Input to Net
+            self.Net.train()
+            edit_colormap, edit_opacity = self.Net(self.content_img_aug)
 
-            target_features = clip_model.encode_image(utils.img_normalize_clip(self.target, self.device))
-            target_features /= (target_features.clone().norm(dim=-1, keepdim=True))
-            grob_direction = target_features - source_features
-            grob_direction /= grob_direction.clone().norm(dim=-1, keepdim=True)
-            grob_loss = 1 - torch.cosine_similarity(grob_direction, text_direction, dim=1)
-            grob_loss = grob_loss.mean()
+            #Extract features
+            #self.requires_grad_(True)
+            self.editedimg = edit_opacity*edit_colormap + (1-edit_opacity)*self.content_img_aug
+            out_features = clip_model.encode_image(utils.img_normalize_clip(self.editedimg, self.device))
+            out_features /= out_features.clone().norm(dim=-1, keepdim=True)
+            img_direction = out_features - source_features
+            img_direction /= img_direction.clone().norm(dim=-1, keepdim=True)
 
-            loss = self.lambda_grob*grob_loss + self.lambda_content*content_loss + self.lambda_patch*patch_loss
+            #Composition loss
+            cosine_loss = 1 - torch.cosine_similarity(out_features, text_features)
+            direction_loss = 1 - torch.cosine_similarity(img_direction, text_direction)
+            composition_loss = cosine_loss + direction_loss
+
+            #Screen loss
+            self.editedimg_screen = edit_opacity*edit_colormap + (1-edit_opacity)*self.green_img
+            out_features_screen = clip_model.encode_image(
+                utils.img_normalize_clip(self.editedimg_screen, self.device),
+                )
+            out_features_screen /= out_features_screen.clone().norm(dim=-1, keepdim=True)
+            screen_loss = 1 - torch.cosine_similarity(out_features_screen, text_features_screen)
+
+            #Structure loss
+            source_tokens = clip_model.encode_image(
+                utils.img_normalize_clip(self.content_img_aug, self.device), 
+                return_spatial_tokens=True,
+                )
+            out_tokens = clip_model.encode_image(
+                utils.img_normalize_clip(self.editedimg, self.device), 
+                return_spatial_tokens=True,
+                )
+            source_similarity_matrix = utils.calc_clip_similarity_matrix(source_tokens, self.device)
+            source_similarity_matrix /= source_similarity_matrix.clone().norm(dim=-1, keepdim=True)
+            out_similarity_matrix = utils.calc_clip_similarity_matrix(out_tokens, self.device)
+            out_similarity_matrix /= out_similarity_matrix.clone().norm(dim=-1, keepdim=True)
+            # print(source_similarity_matrix.shape)
+            Frobenius_norm_distance = torch.sum((source_similarity_matrix - out_similarity_matrix)**2)
+            structure_loss = Frobenius_norm_distance
+
+            loss = composition_loss*self.lambda_composition+screen_loss*self.lambda_screen+structure_loss*self.lambda_structure
+
             loss.backward()
             self.optimizer.step()
 
             losses["total_loss"] = loss.item()
-            losses["content_loss"] = content_loss.item()
-            losses["grobal_loss"] = grob_loss.item()
-            losses["patch_loss"] = patch_loss.item()
+            losses["screen_loss"] = screen_loss.item()
+            losses["structure_loss"] = structure_loss.item()
+            losses["composition_loss"] = composition_loss.item()
+            print(self.scheduler.get_last_lr())
+            losses["lr"] = self.scheduler.get_last_lr()[0]
             if self.wandb:
+                editedimg = wandb.Image(self.editedimg.detach().cpu().numpy().squeeze(0).transpose(1,2,0))
+                contentimg = wandb.Image(self.content_img_aug.detach().cpu().numpy().squeeze(0).transpose(1,2,0))
                 wandb.log(losses)
+                if self.log_picture:
+                    if step % 30==0:
+                        wandb.log({"content":contentimg,"output":editedimg})
 
         # self.save()
         self.inference()
