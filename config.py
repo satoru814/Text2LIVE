2e6f2c9a169b635e88d4c85261f77bc473413530
satoru814 <sakamotosatoru93@gmail.com>
2022-07-27 12:03:44 +0900

    update

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ class CFG:
             "name":"test",
             "notes":"test"}
     lr = 2.5e-3
-    content_path = "./data/ship.jpeg"
+    content_path = "./data/bear.jpeg"
     save_inference_path = "./outs/inference.png"
     img_size = 512
-    text = "Ship on fire"
-    screen = "Fire over the green screen"
-    ROI = "Ship"
-    step = 200
+    text = "Bear on fire"
+    screen = "fire over the green screen."
+    ROI = "bear"
+    step = 500
     optimizer_params = {
         "lr":2.5e-3,
         "momentum":0.9,
@@ -21,3 +21,4 @@ class CFG:
     lambda_composition = 1
     lambda_screen = 1
     lambda_structure = 2
+    lambda_reg = 2 * 5e-2
